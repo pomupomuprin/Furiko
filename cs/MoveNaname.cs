51007f78a1f98c0c729baf66aae471439881d7a5
hamasaki <pomupomuprinn@yahoo.co.jp>
2015-03-03 00:35:36 +0900

    first commit

---
+++ cs/MoveNaname.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveNaname : MonoBehaviour {
+
+	bool muki = true;		
+	void  Update (){
+		
+		if (muki) 
+		{
+			transform.position = new Vector3(transform.position.x + 0.05F, transform.position.y,transform.position.z);
+			transform.eulerAngles = new Vector3(0, 0, -45);
+			//transform.eulerAngles.z = 45.0f;
+			//transform.Translate(0.05f, 0, 0);
+			//transform.Rotate(45, 90, 180);
+			//transform.position.x += 0.05f;
+			//transform.rotation.z = -0.45f;
+		}
+		else
+		{
+			transform.position = new Vector3(transform.position.x - 0.05F, transform.position.y,transform.position.z);
+			//transform.eulerAngles.z = -45.0f;
+			transform.eulerAngles = new Vector3(0, 0, 45);
+
+		}
+		
+		if (transform.position.x > 7.75f)
+		{
+			muki = false;
+		}
+		if (transform.position.x < -7.75f)
+		{
+			muki = true;
+		}
+	}
+
+}
